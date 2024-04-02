93dcd2ff1a2eb6ea401e8ac1643af2b8e9ade47c
pedrocasxz0110 <135608813+pedrocasxz0110@users.noreply.github.com>
2024-04-02 13:11:15 -0300

    Create jsEX07.js

---
+++ LISTAEX/jsEX07.js
@@ -0,0 +1,10 @@
+const numero1 = parseFloat(prompt("Número 1: "));
+const numero2 = parseFloat(prompt("Número 2: "));
+const numero3 = parseFloat(prompt("Número 3: "));
+
+let maior = numero1;
+
+if (numero2 > maior) maior = numero2;
+if (numero3 > maior) maior = numero3;
+
+console.log("O maior é:", maior);
